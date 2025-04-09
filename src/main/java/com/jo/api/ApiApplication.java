43c571a5afb01701dd7153233644f8f6cbe5dff2
Axel Teisseire <mr.teisseire@gmail.com>
2025-04-09 17:02:42 +0200

    add spring boot project

---
+++ src/main/java/com/jo/api/ApiApplication.java
@@ -0,0 +1,14 @@
+package com.jo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	// okok
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
