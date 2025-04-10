d3525525c175f65de20fde3db3f056dd943a6dbd
Axel Teisseire <mr.teisseire@gmail.com>
2025-04-10 15:56:18 +0200

    add new Service for BookingOffer and change endpoints in BookingOfferController

---
+++ src/main/java/com/jo/api/service/BookingOfferService.java
@@ -0,0 +1,27 @@
+package com.jo.api.service;
+
+import com.jo.api.pojo.BookingOffer;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class BookingOfferService {
+
+    private final List<BookingOffer> bookingOffers = new ArrayList<>();
+
+    private final AtomicLong counter = new AtomicLong(1);
+
+    public List<BookingOffer> getAllBookingOffers() {
+        return bookingOffers;
+    }
+
+    public BookingOffer getBookingOfferByTitle(String title) {
+        return bookingOffers.stream()
+                .filter(bookingOffer -> bookingOffer.getTitle().equals(title))
+                .findAny()
+                .orElse(null);
+    }
+}
--- src/main/java/com/jo/api/ws/BookingOfferController.java
+++ src/main/java/com/jo/api/ws/BookingOfferController.java
@@ -1,15 +1,38 @@
 package com.jo.api.ws;
 
+import com.jo.api.pojo.BookingOffer;
+import com.jo.api.service.BookingOfferService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping( value = ApiRegistration.API + ApiRegistration.REST_BOOKINGOFFER)
 public class BookingOfferController {
 
+    @Autowired
+    private BookingOfferService bookingOfferService;
+
+    /**
+     * retourne toutes les offres de réservation
+     * @return une liste d'offre de réservation
+     */
     @GetMapping
-    public String helloWorld() {
-        return "Hello World";
+    public List<BookingOffer> getAllBookingOffers() {
+        return bookingOfferService.getAllBookingOffers();
+    }
+
+    /**
+     *
+     * retourne une seule offre de réservation
+     * @return une offre de réservation
+     */
+    @GetMapping("/{title}")
+    public BookingOffer getBookingOfferByTitle(@PathVariable("title") String title) {
+        return bookingOfferService.getBookingOfferByTitle(title);
     }
 }
