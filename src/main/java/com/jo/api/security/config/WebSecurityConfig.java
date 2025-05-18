4a2f9703b6412f2a05280558814a04661c2db6cc
Axel Teisseire <mr.teisseire@gmail.com>
2025-05-18 17:24:37 +0200

    put new address in security config for app front in prod

--- src/main/java/com/jo/api/security/config/WebSecurityConfig.java
+++ src/main/java/com/jo/api/security/config/WebSecurityConfig.java
@@ -67,7 +67,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3000/GestionDesOffres"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3000/GestionDesOffres","https://jeux-olympiques-79sb.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
